6c612048fff74044a079a691b72b660e9d5de387
Bonfanti Cecilia <bonfanticeciliaemilce@gmail.com>
2022-12-19 10:59:20 -0300

    Fueron integrados mobile-menu y desktop-menu de forma responsive

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const iconMenu = document.querySelector(".menu");
 
-menuEmail.addEventListener('click', toggleDesktopMenu)
+menuEmail.addEventListener('click', toggleDesktopMenu);
+iconMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
    desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+   mobileMenu.classList.toggle('inactive')
 }
